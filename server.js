86bad157a72e3c8a34393bf1f656405ef1a7c2ed
Apjuve <abiponce3@hotmail.com>
2022-03-19 18:14:50 -0400

    add routes for homepage

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 const express = require('express');
+const res = require('express/lib/response');
+const { readFile, read } = require('fs');
 const path = require('path');
-const { clog } = require('./middleware/clog');
 const api = require('./routes/index.js');
+const database = './db/db.json';
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+
+
+app.use(express.json());
+app.use(express.static({extended: true}));
+
+app.get ('/', (req, res) => {
+    res.sendFile(path.join(_dirname, '/public/index.html'))
+});
+
+app.get ('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
+app.get('/api/notes', (req, res) => {
+    readFile('/db/db.json').then((data) => res.json(JSON.parse(data)))
+});
