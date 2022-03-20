cedad6148d12df9229a61f34723357fb6c20729f
Apjuve <abiponce3@hotmail.com>
2022-03-19 20:53:12 -0400

    begin writing write to file function

--- helpers/util.js
+++ helpers/util.js
@@ -1,2 +1,11 @@
+const { Console } = require('console');
 const fs = require('fs');
 const util = require('util');
+
+// promise version of fs.readFile
+const readFromFile = util.promisify(fs.readFile);
+
+const writeToFile = (destination, content) => 
+fs.writeFile(destination, JSON.stringify(content, null, 4), (err) => 
+err ? console.error(err) : Console.info (`\nData wriitten to ${destination}`)
+);
