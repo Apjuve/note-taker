7b6a5042f0ae51ca7cade5a09f24f4cca7f2a93e
Apjuve <abiponce3@hotmail.com>
2022-03-19 21:19:12 -0400

    finish read and append function

--- helpers/util.js
+++ helpers/util.js
@@ -9,3 +9,15 @@ const writeToFile = (destination, content) =>
 fs.writeFile(destination, JSON.stringify(content, null, 4), (err) => 
 err ? console.error(err) : Console.info (`\nData wriitten to ${destination}`)
 );
+
+const readAndAppend = (content, file) => {
+    fs.readFile(file, 'utf8', (err,data) => {
+        if(err) {
+            console.error(err);
+        }else {
+            const parsedData = JSON.parse(data);
+            parsedData.push(content);
+            writeToFile(file, parsedData);
+        }
+    });
+};
